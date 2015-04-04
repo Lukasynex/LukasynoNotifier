e021d3a10278c33292bc0e86b818623a9b81440c
Lukasz <lukmar993@gmail.com>
2015-04-04 23:57:45 +0200

    calendar alarm created

--- src/com/lukasynonotifier/alarms/CalendarActivity.java
+++ src/com/lukasynonotifier/alarms/CalendarActivity.java
@@ -1,10 +1,13 @@
 package com.lukasynonotifier.alarms;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import android.app.AlarmManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.lukasynonotifier.events.Player;
 import com.lukasynonotifier.setup.R;
@@ -23,7 +26,27 @@ public class CalendarActivity extends AbstractAlarmActivity {
 
 	@Override
 	public void prepareAlarm(View v) {
-		// TODO Auto-generated method stub
+		final TimePicker timepicker = (TimePicker) findViewById(R.id.timePicker1);
+		final DatePicker datepicker = (DatePicker) findViewById(R.id.datePicker1);
+		timepicker.clearFocus();
+		int hours = timepicker.getCurrentHour();
+		int minutes = timepicker.getCurrentMinute();
+
+		int dayOfMonth = datepicker.getDayOfMonth();
+		int year = datepicker.getYear();
+		int month = datepicker.getMonth();
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.set(Calendar.MINUTE, minutes);
+		calendar.set(Calendar.HOUR, hours);
+
+		calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.YEAR, year);
+		
+		date = calendar.getTime();
+		
+		manager.set(AlarmManager.RTC, date.getTime(), pendingIntent);
 	}
 
 }
